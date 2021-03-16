ff0d11a10fec170d910bce1f28a04d81f73868d0
Torontow <mtorontow0@gmail.com>
2021-03-15 20:46:28 -0400

    Imported components and pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+
+import Book from "./components/Book"
+import Container from "./components/Container"
+import Nav from "./components/Nav"
+import SearchBar from "./components/SearchBar"
+import Title from "./components/Title"
+
+import Saved from "./pages/Saved"
+import Search from "./pages/Search"
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
     </div>
   );
 }
